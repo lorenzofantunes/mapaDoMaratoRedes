32169f0c78f9bc2ff6769aeb82502b2751933106
lorenzo <fantunes.lorenzo@gmail.com>
2018-07-18 10:30:35 -0300

    server com threads

--- serverPython/client.py
+++ serverPython/client.py
@@ -1,26 +1,42 @@
+import threading
 import socket
 import json
+import sys
 
-HOST = '127.0.0.1'  # Endereco IP do Servidor
+#HOST = '127.0.0.1'  # Endereco IP do Servidor
+HOST = '20.1.1.113'  # Endereco IP do Servidor
 PORT = 5000            # Porta que o Servidor esta
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 dest = (HOST, PORT)
-print('Para sair use CTRL+X\n')
+
+"""'onde': {
+'coordinates': ('-31.781023', '-52.323445')
+},"""
 
 sessao = {
-    'id': '12345',
     'palavras': ['computacao', 'programadoreshtml'],
-    'onde': ('-31.781023', '-52.323445'),
-    'nome': 'Lorenzo F. Antunes'
+    'onde': 'Pair{-31.781023, -52.323445}',
+    'nome': sys.argv[1]
 }
 
-msg = input()
+#msg = bytes(sys.argv[1].encode())
 msg = json.dumps(sessao)
 msg = bytes(msg.encode())
 
-while msg != '\x18':
+def setInterval(func, time):
+    e = threading.Event()
+    while not e.wait(time):
+        func()
+
+def foo():
+    print("sent", sys.argv[1])
     udp.sendto (msg, dest)
-    msg = input()
-    msg = json.dumps(sessao)
-    msg = bytes(msg.encode())
-udp.close()
+    test, cliente = udp.recvfrom(1024)
+    print(test)
+
+setInterval(foo, 1)
+
+
+
+
+#udp.close()
